95cf38ea4545088e66aef69ad94f4ff38404fdd1
natmatsumoto <pea723@users.noreply.github.com>
2021-09-06 09:11:07 +0900

    fix: fix typo

--- src/trace.js
+++ src/trace.js
@@ -175,7 +175,7 @@ export default {
 
     var xScaleType = chart.config.options.scales.x.type
 
-    if (xScaleType !== 'linear' && xScaleType !== 'time' && xScaleType !== 'category' && xScaleType !== 'logarithmic' && xScaleType !== 'timeseries) {
+    if (xScaleType !== 'linear' && xScaleType !== 'time' && xScaleType !== 'category' && xScaleType !== 'logarithmic' && xScaleType !== 'timeseries') {
       return;
     }
 
